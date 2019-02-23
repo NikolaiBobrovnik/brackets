d76ba34459632f1cf93d7a917af31857cae0343a
Николай <n.bobrovnik@aic.ru>
2019-02-23 04:11:39 +0300

    end commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  var chars = str.split(''),
+    bracketsConfig = [],
+    open = ['{', '(', '['],
+    close = ['}', ')', ']'],
+    closeIndex,
+    openIndex;
+
+  // Проходимся по строке, проверяя каждый ее символ.
+  for (let i = 0, len = chars.length; i < len; i++) {
+    openIndex = open.indexOf(chars[i]);
+    if (openIndex !== -1) {
+      // Нашли открывающую скобку. Помещаем ее в стек.
+      bracketsConfig.push(openIndex);
+      continue;
+    }
+
+    closeIndex = close.indexOf(chars[i]);
+    if (closeIndex !== -1) {
+      // Нашли закрывающую скобку. Проверяем ее соответствие открывающей (п.3).
+      openIndex = bracketsConfig.pop();
+      if (closeIndex !== openIndex) {
+        return false;
+      }
+    }
+  }
+
+  // Проверяем дисбаланс открытых/закрытых скобок.
+  if (bracketsConfig.length !== 0) {
+    return false;
+  }
+
+  return true;
+};
